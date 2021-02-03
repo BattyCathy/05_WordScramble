ebb5d7f308399040826c5f8270ca3c25829bd145
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-03 00:27:47 -0800

    Update ContentView.swift

--- 05_WordScramble/ContentView.swift
+++ 05_WordScramble/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make srue we don't add duplicate words with case differencce
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //exit if the remaining string is empty
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // exter validation to come
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
     }
 }
 
