d1b7fc6cb79a8c93c6a66153206c8b6c2c6752f9
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-02 19:35:48 -0800

    TOC + Intro Added

---
+++ 05_WordScramble/Notes.swift
@@ -0,0 +1,29 @@
+//
+//  Notes.swift
+//  05_WordScramble
+//
+//  Created by Austin Roach on 2/2/21.
+//
+
+import Foundation
+
+
+//MARK: Project 5, Part 1
+
+//It's time for another new project, and this is actualy the last easy project we have of this course - after this the difficulty level ramps up a little as we tackle bigger apps, so enjoy this while it lasts!
+
+//In this app you'll work with two of the real fundamentals of app development: List for working with tables of data, and strings, for handling text. Yes, we covered strings wuite a bit alread, but now we're really going to dig into them, including how to work with their Unicode representation so we can get compatibility with older Objective-C frameworks.
+
+//Unicode is a standard for storing and representing text, which at first glance you might think sounds esy. But trust me on this: it really isn't. You know how I seaid sates are hard? Well, dates are easy compared to storing text properly. In fact, there's even a joke mug you can buy that says "I ? Unicode" - a painful reminder that when text representation goes bad all you see is a question mark where the symbol should be.
+
+//Today you have four topics to work through and you'll meet List, Bundle, UITextChecker, and more.
+
+// 1. Word Scramble: Introduction
+
+// 2. Introducing List, Your Best Friend
+
+// 3. Loading Resources From Your App Bundle
+
+// 4. Working with Strings
+
+
