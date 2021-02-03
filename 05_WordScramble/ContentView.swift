10fd1899a7df17cd456ed213bd96ff25e2bfe6a9
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-03 00:31:44 -0800

    Update ContentView.swift
    Added character count

--- 05_WordScramble/ContentView.swift
+++ 05_WordScramble/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+<<<<<<< Updated upstream
         Text("Hello, world!")
             .padding()
+=======
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .autocapitalization(.none)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+>>>>>>> Stashed changes
     }
 }
 
