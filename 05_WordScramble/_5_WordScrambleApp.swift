f14a60c693a8ece3c3a77d597c40ce0b9628461b
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-02 19:25:56 -0800

    Initial Commit

---
+++ 05_WordScramble/_5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_WordScrambleApp.swift
+//  05_WordScramble
+//
+//  Created by Austin Roach on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct _5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
