1b68f86ccbcabd43cefd7e999aad0a9138fb66a0
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-02 19:41:13 -0800

    Update Notes.swift
    Intro transcribed

--- 05_WordScramble/Notes.swift
+++ 05_WordScramble/Notes.swift
@@ -20,6 +20,15 @@ import Foundation
 
 // 1. Word Scramble: Introduction
 
+//This project will be another game, although really it's just a sneaky way for me to introduce more Swift and SWiftUI knowledge! The game will show players a random eight-letter work, and askk them to make words out of it. For example, if the starter word is "alarming" they might spell "alarm", "ring", "main", and so on.
+
+//Along the way you'll meet List, onAppear(), Bundle, fatalError(), adn more - all useful skills that you'll use for years to come. You'll also get some practice with @State, Alert, NavigationView, and more, which you should enjoy while you can - this is our last easy project!
+
+//To get started, please create a new Single View App project called WordScramble. You'll need to downlod the files for this project, as it contains a file called start.txt that you'll be needing later on.
+
+//Okay, oet's get into some code.
+
+
 // 2. Introducing List, Your Best Friend
 
 // 3. Loading Resources From Your App Bundle
